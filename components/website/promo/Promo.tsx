0f0477ddbda28294f21603af2167c13272cb316d
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2022-11-19 23:04:02 +0600

    Fixed conflict and Header component

--- components/website/promo/Promo.tsx
+++ components/website/promo/Promo.tsx
@@ -6,25 +6,25 @@ import React from 'react';
 import styles from '../../../styles/scss/promo.module.scss';
 
 interface PromoProps {
-	bgColor?: string;
-	children: any;
-	imgClassName?: string;
-	imgSrc: string;
+  bgColor?: string;
+  children: any;
+  imgClassName?: string;
+  imgSrc: string;
 }
 
 export default function Promo({ bgColor, children, imgClassName, imgSrc }: PromoProps) {
-	return (
-		<section className={`${bgColor} ${styles.promo}`}>
-			<div className='container mx-auto pl-4 pr-4'>
-				<div className='flex flex-col md:flex-row items-center relative'>
-					<div className='md:basis-1/2 md:pr-4 mb-8 md:mb-0'>{children}</div>
-					<div className='md:basis-1/2 md:pl-4'>
-						<div className={imgClassName}>
-							<Image src={imgSrc} alt='Kyrgyzstan map' width={690} height={339} />
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className={`${bgColor} ${styles.promo}`}>
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row items-center relative">
+          <div className="md:basis-1/2 md:pr-4 mb-8 md:mb-0">{children}</div>
+          <div className="md:basis-1/2 md:pl-4">
+            <div className={imgClassName}>
+              <Image src={imgSrc} alt="Kyrgyzstan map" width={690} height={339} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
