ab0646cb0a8186ef769f16c6fb1cb7d2482fc0b1
murat <jumasheff@gmail.com>
2023-01-24 10:49:49 +0600

    Add GoogleProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 /* External dependencies */
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { Roboto } from '@next/font/google';
@@ -13,14 +14,14 @@ const roboto = Roboto({
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <style jsx global>{`
         html {
           font-family: ${roboto.style.fontFamily};
         }
       `}</style>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 };
 
