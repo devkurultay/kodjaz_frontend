64f49d328dcc24d58bf2e3de714fc4ead4597ca7
murat <jumasheff@gmail.com>
2023-01-24 10:49:30 +0600

    if authenticated in admin pages

--- components/layout/AdminLayout.tsx
+++ components/layout/AdminLayout.tsx
@@ -1,16 +1,27 @@
 /* External dependencies */
 import React from 'react';
+import { useSession } from 'next-auth/react';
 
 /* Local dependencies */
 import FooterAdmin from '../footer-admin/FooterAdmin';
 import HeaderAdmin from '../header-admin/HeaderAdmin';
 import { LayoutProps } from './type';
+import Layout from './Layout';
+import AccessDenied from '../common/AccessDenied';
 
 export default function AdminLayout({ children }: LayoutProps) {
+  const { data: session, status } = useSession();
+
   return (
     <>
       <HeaderAdmin />
-      <main>{children}</main>
+      {session ? (
+        <main>{children}</main>
+      ) : (
+        <Layout>
+          <AccessDenied />
+        </Layout>
+      )}
       <FooterAdmin />
     </>
   );
