6d9960fe50843430f7c782ef5a4780740f5a6f80
murat <jumasheff@gmail.com>
2023-01-24 10:55:53 +0600

    If session, show session.user.email

--- components/website/header/Header.tsx
+++ components/website/header/Header.tsx
@@ -1,4 +1,5 @@
 // External dependencies
+import { useSession } from 'next-auth/react';
 import { Popover, Transition } from '@headlessui/react';
 import Link from 'next/link';
 import { Trans } from 'next-i18next';
@@ -17,6 +18,7 @@ import {
 
 export default function Header() {
   const dispatch = useAppDispatch();
+  const { data: session, status } = useSession();
   const { user } = useAppSelector(userState);
 
   function openPopupLogin() {
@@ -33,7 +35,7 @@ export default function Header() {
         <div className="flex items-center justify-between border-gray-100 py-6 lg:justify-start lg:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <Link href="/" className="w-auto">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">kodjaz</span>
               <LogoIcon width={152} height={40} />
             </Link>
           </div>
@@ -63,9 +65,9 @@ export default function Header() {
             </Popover>
           </Popover.Group>
           <div className="hidden items-center justify-end lg:flex lg:flex-1 lg:w-0">
-            {user ? (
+            {session ? (
               <div>
-                <p>{user.email}</p>
+                <p>{session.user?.email}</p>
               </div>
             ) : (
               <>
@@ -119,9 +121,9 @@ export default function Header() {
             </div>
             <div className="space-y-6 py-8 px-5">
               <div className="flex flex-col justify-end">
-                {user ? (
+                {session ? (
                   <div>
-                    <p>{user.username}</p>
+                    <p>{session.user?.email}</p>
                   </div>
                 ) : (
                   <>
