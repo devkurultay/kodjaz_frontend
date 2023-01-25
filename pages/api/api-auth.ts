88ad1707bba8ce7b81999846dfc98b88ef92d8a6
murat <jumasheff@gmail.com>
2023-01-25 10:32:45 +0600

    Revert axios-related changes

--- pages/api/api-auth.ts
+++ pages/api/api-auth.ts
@@ -1,28 +1,16 @@
 /* Local dependencies */
 import { Login } from '../../types/userTypes';
-import { $api, setTokensToCookies } from './axois-api';
+import { $api, setTokens } from './axois-api';
 
 export async function register(data: any) {
   const response = await $api.post('registration/', data);
   const newAccessToken = response?.data?.access;
   const newRefreshToken = response?.data?.refresh;
 
-  setTokensToCookies(newAccessToken, newRefreshToken);
+  setTokens(newAccessToken, newRefreshToken);
   $api.defaults.headers.Authorization = 'JWT ' + newAccessToken;
 
   return response;
-}
-
-export function performIsAuthCheck(accessToken: string, refreshToken: string) {
-  if (accessToken && refreshToken) {
-    const tokenParts = JSON.parse(atob(refreshToken.split('.')[1]));
-    // exp date in token is expressed in seconds, while now() returns milliseconds:
-    const now = Math.ceil(Date.now() / 1000);
-    if (tokenParts.exp > now) {
-      return true;
-    }
-  }
-  return false;
 }
 
 export async function confirmEmail(key: string) {
@@ -34,7 +22,7 @@ export async function login(data: Login) {
   const newAccessToken = response?.data?.access;
   const newRefreshToken = response?.data?.refresh;
 
-  setTokensToCookies(newAccessToken, newRefreshToken);
+  setTokens(newAccessToken, newRefreshToken);
   $api.defaults.headers.Authorization = 'JWT ' + newAccessToken;
 
   return response;
--- pages/api/axois-api.ts
+++ pages/api/axois-api.ts
@@ -1,125 +1,27 @@
-import axios, { AxiosResponse, AxiosError, AxiosInstance } from 'axios';
+/* External dependencies */
+import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || 'https://api.kodjaz.com/api/';
 
-const isTokenExpired = (errorResp: AxiosResponse) => {
-  const { data, status } = errorResp;
-  const { detail } = data;
-  const errorMsgs = [
-    'Given token not valid for any token type',
-    'Token has been expired.',
-  ];
-  return status === 401 && errorMsgs.includes(detail);
-};
-
-const getWrongCredentialsErrorMessages = (errorResp: AxiosResponse) => {
-  const { data, status } = errorResp;
-  if (status > 401) {
-    return [];
-  }
-  const { email, password, detail } = data;
-  const errorMsg = [];
-  detail && errorMsg.push(detail);
-  email && errorMsg.push(`Email: ${email?.[0]}`);
-  password && errorMsg.push(`Password: ${password?.[0]}`);
-  return errorMsg;
-};
-
-const getTokensFromCookies = () => {
-  const accessToken = Cookies.get('access_token');
-  const refreshToken = Cookies.get('refresh_token');
-  return { accessToken, refreshToken };
-};
-
-export const setTokensToCookies = (
-  access_token: string,
-  refresh_token: string,
-) => {
-  Cookies.set('access_token', access_token);
-  Cookies.set('refresh_token', refresh_token);
-};
-
-export const $api: AxiosInstance = axios.create({
+export const $api = axios.create({
   baseURL: API_URL,
-  headers: {
-    Authorization: Cookies.get('access_token')
-      ? 'Token ' + Cookies.get('access_token')
-      : null,
-    'Content-Type': 'application/json',
-    accept: 'application/json',
-  },
 });
 
-$api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const errorResp = error.response;
-    if (isTokenExpired(errorResp ?? {})) {
-      return refreshTokenAndResendRequest(error);
-    }
-    const wrongCredentialsErrors = getWrongCredentialsErrorMessages(errorResp);
-    if (wrongCredentialsErrors.length > 0) {
-      return Promise.reject(wrongCredentialsErrors);
-    }
-    return Promise.reject(error);
-  },
-);
+export const getHeaders = () => ({
+  'Content-Type': 'application/json; charset=utf-8',
+  Authorization: `Bearer ${Cookies.get('token') ? Cookies.get('token') : ''}`,
+});
 
-let isFetchingAccessTokenInProgress = false;
+export function getTokens() {
+  const refreshToken = Cookies.get('refresh_token');
+  const accessToken = Cookies.get('access_token');
 
-type AccessTokenCallback = (accessToken: string) => void;
+  return { refreshToken, accessToken };
+}
 
-let subscribers: AccessTokenCallback[] = [];
-
-const addSubscriber = (cb: AccessTokenCallback) => {
-  subscribers.push(cb);
-};
-
-const triggerSubscribers = (accessToken: string) => {
-  subscribers.forEach((cb) => cb(accessToken));
-  subscribers = [];
-};
-
-const refreshTokenAndResendRequest = async (error: AxiosError) => {
-  try {
-    const { accessToken, refreshToken } = getTokensFromCookies();
-    if (!accessToken || !refreshToken) {
-      return Promise.reject(error);
-    }
-    const { response: errorResponse } = error;
-    const resendOriginalRequest = new Promise((resolve) => {
-      addSubscriber((token: string) => {
-        if (errorResponse?.config?.headers) {
-          errorResponse.config.headers.Authorization = 'Token ' + token;
-        }
-        if (errorResponse?.config) {
-          resolve(axios(errorResponse.config));
-        }
-      });
-    });
-    if (!isFetchingAccessTokenInProgress) {
-      isFetchingAccessTokenInProgress = true;
-      const response = await axios.post(API_URL + 'token/refresh/', {
-        refresh: refreshToken,
-      });
-      if (response && !response.data) {
-        return Promise.reject(error);
-      }
-      const newAccessToken = response?.data?.access;
-      const newRefreshToken = response?.data?.refresh;
-      await setTokensToCookies(newAccessToken, newRefreshToken);
-      $api.defaults.headers.Authorization = 'Token ' + newAccessToken;
-      isFetchingAccessTokenInProgress = false;
-      triggerSubscribers(newAccessToken);
-    }
-    return resendOriginalRequest;
-  } catch (e) {
-    return Promise.reject(e);
-  }
-};
-
-export default $api;
+export function setTokens(accessToken: string, refreshToken: string) {
+  Cookies.set('access_token', accessToken);
+  Cookies.set('refresh_token', refreshToken);
+}
