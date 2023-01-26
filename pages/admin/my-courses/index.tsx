899ed3ef77f1e641217435edd804cddda149f504
murat <jumasheff@gmail.com>
2023-01-26 10:16:48 +0600

    Pass down subscribed tracks

--- pages/admin/my-courses/index.tsx
+++ pages/admin/my-courses/index.tsx
@@ -1,21 +1,52 @@
 /* External dependencies */
 import Head from 'next/head';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { unstable_getServerSession } from 'next-auth/next';
 import { Trans } from 'next-i18next';
+
 import React from 'react';
 
 /* Local dependencies */
 import MyCourses from '../../../components/education/MyCourses';
 import Layout from '../../../components/layout/Layout';
 import nextI18NextConfig from '../../../next-i18next.config.js';
+import { authOptions } from '../../api/auth/[...nextauth]';
+import { ExtendedSession } from '../../../types/userTypes';
+import { getRequest } from '../../api/axois-api';
+import { getSubscriptions } from '../../api/api-subscription';
 
-export const getStaticProps = async ({ locale }: any) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['common'], nextI18NextConfig)),
-  },
-});
+export async function getServerSideProps(context: any) {
+  // TODO(murat): move this to a helper
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions,
+  );
 
-export default function CoursesPage() {
+  const extSession = session as unknown as ExtendedSession;
+  const subscriptions = await getSubscriptions(extSession.access);
+  const subscribedTracks = await Promise.all(
+    subscriptions.map(
+      // TODO(murat): put proper type
+      async ({ track }: any) =>
+        await getRequest(extSession.access, `v1/user/tracks/${track}/`),
+    ),
+  );
+
+  return {
+    props: {
+      subscribedTracks: subscribedTracks,
+      ...(await serverSideTranslations(
+        context.locale,
+        ['common'],
+        nextI18NextConfig,
+      )),
+    },
+  };
+}
+
+// TODO(murat): put a proper type
+export default function CoursesPage(props: any) {
   return (
     <>
       <Head>
@@ -24,7 +55,7 @@ export default function CoursesPage() {
         <meta property="og:title" content="Kodjaz - Курстар" key="title" />
       </Head>
       <Layout>
-        <MyCourses />
+        <MyCourses subscribedTracks={props?.subscribedTracks ?? []} />
       </Layout>
     </>
   );
