1208c9919c072cf14d929dda443b6de6425ab64b
murat <jumasheff@gmail.com>
2023-01-27 10:38:55 +0600

    Don't use Layout inside ClassroomLayout

--- components/layout/ClassroomLayout.tsx
+++ components/layout/ClassroomLayout.tsx
@@ -6,22 +6,15 @@ import { useSession } from 'next-auth/react';
 import FooterClassroom from '../footer-classroom/FooterClassroom';
 import HeaderClassroom from '../header-classroom/HeaderClassroom';
 import { LayoutProps } from './type';
-import Layout from './Layout';
 import AccessDenied from '../common/AccessDenied';
 
 export default function ClassroomLayout({ children }: LayoutProps) {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
 
   return (
     <>
       <HeaderClassroom />
-      {session ? (
-        <main>{children}</main>
-      ) : (
-        <Layout>
-          <AccessDenied />
-        </Layout>
-      )}
+      {status === 'authenticated' ? <main>{children}</main> : <AccessDenied />}
       <FooterClassroom />
     </>
   );
