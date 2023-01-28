a301d2905bd8f324c4610527825e87fac16dcbbd
murat <jumasheff@gmail.com>
2023-01-28 12:08:58 +0600

    Add entties to store

--- store/slices/trackSlice.ts
+++ store/slices/trackSlice.ts
@@ -3,10 +3,13 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { RootState } from '..';
 import { getRequest, postRequest } from '../../pages/api/axois-api';
 import {
+  EntityById,
   Track,
-  TrackById,
-  TrackByName,
+  EntityByName,
   Subscription,
+  Unit,
+  Lesson,
+  Exercise,
 } from '../../types/tracksTypes';
 
 type TokenAndTrackId = {
@@ -64,14 +67,20 @@ interface userTrackState {
   error?: Error;
   loading: boolean;
   track?: '';
-  tracksById: TrackById;
-  tracksByName: TrackByName;
+  tracksByName: EntityByName<Track>;
+  tracksById: EntityById<Track>;
+  unitsById: EntityById<Unit>;
+  lessonsById: EntityById<Lesson>;
+  exercisesById: EntityById<Exercise>;
 }
 
 const initialState: userTrackState = {
   loading: false,
-  tracksById: {},
   tracksByName: {},
+  tracksById: {},
+  unitsById: {},
+  lessonsById: {},
+  exercisesById: {},
 };
 
 const userTrackSlice = createSlice({
@@ -96,15 +105,35 @@ const userTrackSlice = createSlice({
         state.loading = true;
       })
       .addCase(getTracks.fulfilled, (state, { payload }) => {
-        const tracksById: TrackById = {};
-        const tracksByName: TrackByName = {};
+        const tracksByName: EntityByName<Track> = {};
+        const tracksById: EntityById<Track> = {};
+        const unitsById: EntityById<Unit> = {};
+        const lessonsById: EntityById<Lesson> = {};
+        const exercisesById: EntityById<Exercise> = {};
+
         payload.forEach((track: Track) => {
           tracksById[track.id] = track;
           tracksByName[track.name] = track;
+
+          track.track_units.forEach((unit: Unit) => {
+            unitsById[unit.id] = unit;
+
+            unit.unit_lessons.forEach((lesson: Lesson) => {
+              lessonsById[lesson.id] = lesson;
+
+              lesson.lesson_exercises.forEach((exercise: Exercise) => {
+                exercisesById[exercise.id] = exercise;
+              });
+            });
+          });
         });
+
         state.loading = false;
         state.tracksById = tracksById;
         state.tracksByName = tracksByName;
+        state.unitsById = unitsById;
+        state.lessonsById = lessonsById;
+        state.exercisesById = exercisesById;
       })
       .addCase(getTracks.rejected, (state, { payload }) => {
         state.loading = false;
