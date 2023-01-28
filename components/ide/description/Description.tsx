8aab79a576222a93d4e68b9ab83446366e43770d
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2023-01-28 19:23:42 +0600

    Added menu list

--- components/ide/description/Description.tsx
+++ components/ide/description/Description.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 
 interface DescriptionProps {
   children?: any;
+  title?: string;
 }
 
-export default function Description({ children }: DescriptionProps) {
+export default function Description({ children, title }: DescriptionProps) {
   return (
     <div className="h-full px-5 py-2.5 relative">
-      <h2 className="text-sm2 font-semibold mb-2.5">Strings</h2>
+      <h2 className="text-sm2 font-semibold mb-2.5">{title}</h2>
       <p className="mb-2.5">{children}</p>
     </div>
   );
