f5ce53520d9e892397f4be4cf170731f059391b1
murat <jumasheff@gmail.com>
2023-01-29 23:57:08 +0600

    Show console with errors

--- components/ide/IDE.tsx
+++ components/ide/IDE.tsx
@@ -50,6 +50,8 @@ export default function IDE() {
   const [tabsContent, setTabsContent] = useState<TabContents>([]);
   const [userCode, setUserCode] = useState<string>('');
   const [isConsoleShow, setIsConsoleShow] = useState(false);
+  const [consoleOutput, setConsoleOutput] = useState('');
+  const [consoleError, setConsoleError] = useState('');
   const router = useRouter();
   const { id } = router.query;
   const { data: sessionData, status } = useSession();
@@ -57,6 +59,9 @@ export default function IDE() {
   useEffect(() => {
     if (submission) {
       setUserCode(submission?.submitted_code ?? exercise?.default_code ?? '');
+      setConsoleOutput(submission.console_output);
+      setConsoleError(submission.error_message);
+      setIsConsoleShow(true);
     }
   }, [submission]);
 
@@ -194,81 +199,20 @@ export default function IDE() {
               ></button>
               {isConsoleShow ? (
                 <div
-                  className={`px-7 text-whiteColor overflow-y-scroll max-h-[40vh] lg:max-h-[50vh] w-inherit`}
+                  className={`px-7 text-whiteColor overflow-y-scroll max-h-[40vh] lg:max-h-[50vh] w-inherit ${
+                    submission?.error_message ? 'text-dangerColorConsole' : ''
+                  }`}
                 >
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quos illum obcaecati explicabo dolore vel assumenda
-                    perferendis, impedit omnis saepe deleniti facere, sapiente
-                    hic illo quasi fugit laudantium nobis reiciendis repellat,
-                    dolorem sunt adipisci. Nam vero id cumque sed eos. Quae,
-                    porro explicabo maxime quas beatae nihil accusantium,
-                    molestiae pariatur placeat, quis nesciunt non est odit
-                    libero temporibus quo veniam a? Repellat accusantium eaque
-                    officia velit placeat porro incidunt eum adipisci,
-                    repudiandae ut neque, blanditiis explicabo earum deleniti
-                    numquam similique fugiat. Recusandae illum iusto, atque
-                    inventore autem pariatur voluptates veniam ipsam. Rerum
-                    eaque architecto quae earum eum delectus reiciendis maxime
-                    dolore possimus qui, fugiat et laborum totam tempora libero,
-                    dicta quasi, ipsum suscipit. Fugit pariatur qui neque ad eum
-                    alias vero doloremque quisquam voluptas magnam ex impedit
-                    delectus ratione consectetur quas quam laboriosam amet,
-                    sequi aut repudiandae in eligendi, sit atque exercitationem?
-                    Molestiae doloribus vero assumenda ducimus quas.
-                    Reprehenderit, eum odit cumque aut earum libero soluta harum
-                    nulla quod, reiciendis ab necessitatibus atque quia rem
-                    mollitia blanditiis dolorem unde optio consectetur impedit
-                    veritatis assumenda provident nisi. Nemo iusto corporis
-                    cumque adipisci rem laudantium soluta in eligendi harum
-                    sunt. Illo magnam nihil maxime veritatis beatae molestias
-                    est doloribus eveniet quae quod similique fuga, dignissimos
-                    possimus at modi totam rerum molestiae quia ad aperiam! Cum
-                    explicabo quos consectetur illo unde quia expedita animi?
-                    Architecto excepturi pariatur consectetur temporibus! Atque
-                    et perspiciatis odio alias magnam laborum, magni, dolore
-                    libero doloremque harum fugit. A voluptatibus ut in
-                    excepturi, perspiciatis nobis alias exercitationem cum
-                    deleniti non. Eligendi dignissimos quos commodi. Ut eaque
-                    quos autem voluptatibus perferendis, reiciendis dolores
-                    cumque officiis deserunt excepturi illo, ratione placeat
-                    incidunt, ea magni dignissimos provident sint doloremque
-                    consequuntur. Maxime obcaecati alias fugit molestias commodi
-                    sunt placeat eveniet quam praesentium veritatis enim vero
-                    aspernatur dolorum doloribus repellendus totam sed
-                    temporibus, rerum dolor consectetur facere voluptatum. Dicta
-                    modi itaque excepturi rem, rerum quasi. Obcaecati dolore
-                    consequatur cupiditate ipsam, mollitia asperiores facere
-                    repellat, magnam voluptates similique dolorum corporis!
-                    Asperiores quasi fugiat dolore sit ab deleniti temporibus
-                    distinctio itaque magni, dolorem saepe, consequatur atque
-                    laborum? Dolores ex laudantium nam magni distinctio tenetur
-                    nihil. Blanditiis delectus voluptatem atque minus asperiores
-                    distinctio dolor esse, totam repellat quod nobis iusto,
-                    voluptatum iste vitae eaque quam odit ratione sed! Omnis
-                    obcaecati sunt esse. Earum facere rem saepe sunt iure
-                    aliquam temporibus harum? Repellendus voluptates sit non nam
-                    eveniet rem quaerat nobis exercitationem debitis beatae
-                    quasi esse odio maiores nisi id, animi, ad architecto
-                    adipisci! Eius culpa iste perspiciatis hic omnis voluptatum
-                    velit quibusdam reprehenderit, aliquam dolore maiores magni
-                    esse accusamus optio recusandae inventore vero a totam
-                    eaque, odit molestiae cum id voluptate corporis. Blanditiis,
-                    magni ratione doloremque culpa, qui enim earum impedit autem
-                    id quidem molestias natus recusandae suscipit, dolor cumque
-                    libero soluta quibusdam possimus harum cupiditate accusamus
-                    nihil? At dolores qui officiis corporis aperiam nam,
-                    provident, laborum tenetur iusto necessitatibus soluta rerum
-                    totam reprehenderit eveniet quis! Consequuntur, minima? Id
-                    amet asperiores natus totam at quia reprehenderit quas
-                    fugiat voluptatum beatae vitae itaque est sequi labore ad
-                    eum, odit vel consequatur error culpa perspiciatis tempora?
-                    Repellat praesentium maxime earum minus, odit odio
-                    doloremque molestiae a vero repellendus ex sapiente
-                    distinctio harum porro sequi velit autem enim quam dolorem
-                    assumenda id, error ratione nesciunt? Asperiores, totam
-                    harum.
-                  </p>
+                  <div>
+                    {consoleError && (
+                      <pre dangerouslySetInnerHTML={{ __html: consoleError }} />
+                    )}
+                    {consoleOutput && (
+                      <pre
+                        dangerouslySetInnerHTML={{ __html: consoleOutput }}
+                      />
+                    )}
+                  </div>
                 </div>
               ) : (
                 ''
